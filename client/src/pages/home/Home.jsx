578cef431b5944d307568459606784279b5538da
Iheb Jaouadi <61436895+ihebjaouadi@users.noreply.github.com>
2023-03-06 17:22:00 +0100

    frontend intefraces

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import "./Home.css";
-import SignUp from "../../components/sign-up/signUp";
+import {Link, Outlet, useLocation} from "react-router-dom";
 
 function Home() {
+    const location = useLocation();
     return (
         <section className='home-container'>
             <div className='content-container'>
                 <header className="content-header">
-                <div className="logo"> Eco-Fit </div>
-                    <p>Already have an account ? <span> Sign In</span></p>
+                    <div className="logo">Eco-Fit</div>
+                    {location.pathname === "/sign-in" ? (
+                        <p>
+                            Don't have an account ?<Link className="link" to="/">Sign Up</Link>
+                        </p>
+                        ):(
+                        <p>
+                            Already have an account ? {""} <Link className="link" to="/sign-In"> Sign In</Link>
+                        </p>
+                    )}
                 </header>
                 <div className="outlet-container">
-                    <SignUp/>
+                    <Outlet/>
                 </div>
             </div>
             <div className='image-container'></div>
         </section>
+
     );
 }
 
 export default Home;
+
